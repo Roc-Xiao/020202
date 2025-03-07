c835a4627c3175d03db1f9a1632e15232413f80a
Roc Xiao <916751792@qq.com>
2025-03-07 22:28:26 +0800

    1

--- answer/include/answer/game_config.hpp
+++ answer/include/answer/game_config.hpp
@@ -4,10 +4,10 @@
 namespace game_params {
     const int grid_num_v = 256; // 水平方向网格数量
     const int grid_num_h = 128; // 垂直方向网格数量
-    const int attack_distance = 80; // 攻击距离,敌人在攻击距离内再攻击
-    const double danger_hp = 0.9; // 生命值低于这个值就要补充
-    const int danger_bullet_num = 5; // 子弹低于这个值就要补充
-    const double speed_scale = 1.0 / 32.0; // 速度缩放比例
+    const int attack_distance = 80; // 判断攻击距离
+    const double speed_scale = 1.0 / 32.0; // 速度
+    const double danger_hp = 0.8; // 回城生命值
+    const int danger_bullet_num = 5; // 回城子弹数
 }
 
 #endif // _GAME_CONFIG_HPP_
